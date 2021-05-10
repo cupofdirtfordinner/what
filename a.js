3528f89518768246627b8b479123a89008461bee
cupofdirtfordinner <75740323+cupofdirtfordinner@users.noreply.github.com>
2021-05-10 11:01:27 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         Unblock GoGuardian Blocked websites
+// @namespace    https://gist.github.com/LegusX/9f7724d01f67a1a3137e322be3057ec9/raw/3ee19e79ea68ed01b5c885277705ae54a273e5e5/unblocker.user.js
+// @version      1.3
+// @description  Automatically unblocks any websites that are blocked by GoGuardian that start with http://
+// @author       LegusX
+// @match        http://blocked.com-default.ws/*
+// @grant        none
+// @run-at document-start
+// ==/UserScript==
 (function() {
     'use strict';
     window.stop();
